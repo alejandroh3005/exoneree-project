964f0cd50eac7050da406ad5bc1fbde250b40fd9
alejandroh3005 <alejandroh3005@gmail.com>
2024-07-16 10:47:43 -0700

    Update feature engineering and verify covariates as suitable

--- feature-engineering.R
+++ feature-engineering.R
@@ -1,10 +1,11 @@
 #' Title: "Feature engineering script"
 #' Project: "Exoneree Project"
 #' Author: "Alejandro Hernandez"
-#' Date: "May 24, 2024"
+#' Date: "May 26, 2024"
 
 rm(list = ls()) # clear workspace
 library(dplyr)
+library(tidyr)
 library(lubridate)
 
 # load data
@@ -20,11 +21,55 @@ exonerees <- exonerees %>%
   # create ID
   dplyr::mutate(ID = dplyr::row_number()) %>%
   dplyr::select(ID, everything()) %>%
-  # convert and rename Year of Crime
+  # convert and rename 'Date of Crime Year' to 'Year of Crime'
   mutate(Date.of.Crime.Year = as.numeric(gsub(",", "", Date.of.Crime.Year))) %>%
   dplyr::rename(Year.of.Crime = Date.of.Crime.Year) %>%
-  # rename Age at Crime
+  # rename 'Age' to 'Age at Crime'
   rename(Age.at.Crime = Age)
+
+
+violent_felonies <- c(
+  "Murder", "Manslaughter", "Attempted Murder", "Accessory to Murder", 
+  "Sexual Assault", "Child Sex Abuse", "Child Abuse", "Robbery", "Assault", 
+  "Arson", "Kidnapping", "Terrorism", "Supporting Terrorism", 
+  "Attempt, Violent", "Other Violent Felony")
+misdemeanors <- c(
+  "Burglary/Unlawful Entry", "Theft", "Forgery", 
+  "Possession of Stolen Property", "Destruction of Property", 
+  "Drug Possession or Sale", "Weapon Possession or Sale", 
+  "Other Violent Misdemeanor", "Other Nonviolent Misdemeanor")
+nonviolent_felonies <- c(
+  "Sex Offender Registration", "Tax Evasion/Fraud", "Immigration", "Fraud", 
+  "Bribery", "Perjury", "Official Misconduct", "Traffic Offense", "Conspiracy", 
+  "Solicitation", "Obstruction of Justice", "Failure to Pay Child Support", 
+  "Dependent Adult Abuse", "Attempt, Nonviolent", "Other Nonviolent Felony",
+  "Military Justice Offense")
+misconduct <- c(
+  "Menacing", "Stalking", "Harassment", "Threats", "Filing a False Report", 
+  "Other")
+
+exonerees <- exonerees %>%
+  mutate(
+    Crime.Category = 
+      ifelse(Worst.Crime.Display %in% violent_felonies, "Violent felony",
+      ifelse(Worst.Crime.Display %in% misdemeanors, "Misdemeanor",
+      ifelse(Worst.Crime.Display %in% nonviolent_felonies,"Nonviolent felony",
+      ifelse(Worst.Crime.Display %in% misconduct, "Misconduct",
+             "Other")))))
+
+# Make a table of each offense and the corresponding crime category it falls into
+
+all_offenses <- c(violent_felonies, misdemeanors, nonviolent_felonies, misconduct)
+
+list_names <- c(rep("Violent Felony", length(violent_felonies)),
+                rep("Misdemeanor", length(misdemeanors)),
+                rep("Nonviolent Felony", length(nonviolent_felonies)),
+                rep("Misconduct", length(misconduct)))
+
+# Create the data frame
+crime_table <- data.frame(Category = list_names, 
+                          Offense = all_offenses,
+                          stringsAsFactors = FALSE)
 
 
 ### ============================= ###
@@ -110,6 +155,14 @@ exonerees <- exonerees %>%
     Years.Convicted = floor(Months.Convicted / 12))
     # todo: time served in prison
 
+exonerees <- exonerees %>%
+  mutate(
+    Time.Incarcerated =
+           ifelse(Months.Incarcerated >= 300, "25 or more years",
+           ifelse(Months.Incarcerated >= 120, "10 to 25 years",
+           ifelse(Months.Incarcerated >= 12, "1 to 10 years",
+                  "Less than 1 year"))))
+
 
 ### ============================= ###
 ### Derive new demographics
@@ -124,6 +177,20 @@ exonerees <- exonerees %>%
     # Age at exoneration = (Age at conviction) + (Years convicted)
     Age.at.Exoneration = Age.at.Conviction + Years.Convicted,
     )
+
+age_groups = c(0, 18, 30, 40, 65, 100)
+age_labels = c("Minor", "Young adult", "Adult", "Middle age", "Senior")
+
+exonerees <- exonerees %>% 
+  mutate(
+    Age.Group.at.Crime = cut(Age.at.Crime, breaks = age_groups,
+                             labels = age_labels, include.lowest = TRUE),
+    Age.Group.at.Conviction = cut(Age.at.Conviction, breaks = age_groups,
+                               labels = age_labels, include.lowest = TRUE),
+    Age.Group.at.Release = cut(Age.at.Release, breaks = age_groups,
+                             labels = age_labels, include.lowest = TRUE),
+    Age.Group.at.Exoneration = cut(Age.at.Exoneration, breaks = age_groups,
+                               labels = age_labels, include.lowest = TRUE))
 
 
 ### ============================= ###
@@ -146,11 +213,13 @@ exonerees %>%
 exonerees_demographics <- exonerees %>% 
   select(Last.Name, First.Name, 
          Race, Sex, 
-         Age.at.Crime, Age.at.Conviction, Age.at.Release, Age.at.Exoneration)
+         Age.at.Crime, Age.at.Conviction, Age.at.Release, Age.at.Exoneration,
+         Age.Group.at.Crime, Age.Group.at.Conviction, 
+         Age.Group.at.Release, Age.Group.at.Exoneration)
 
 # separate conviction information
 exonerees_conviction <- exonerees %>%
-  select(State, County, Worst.Crime.Display, 
+  select(State, County, Worst.Crime.Display, Crime.Category,
          Year.of.Crime, Year.of.1st.Conviction)
 
 # separate exoneration information
@@ -164,21 +233,24 @@ exonerees <- exonerees %>% select(
   # demographics
   ID, Last.Name, First.Name, Race, Sex, 
   # crime details
-  State, County, Worst.Crime.Display, Sentence,
+  State, County, Worst.Crime.Display, Crime.Category, Sentence,
   # conviction and exoneration tags
   existing_extags, new_contags,
   # time served
-  Months.Incarcerated, Months.Convicted, Years.Incarcerated, Years.Convicted,
+  Time.Incarcerated, Months.Incarcerated, Months.Convicted, Years.Incarcerated, Years.Convicted,
   # year of events
   Year.of.Crime, Year.of.1st.Conviction, Year.of.Release, Year.of.Exoneration,
   # age at events
-  Age.at.Crime, Age.at.Conviction, Age.at.Release, Age.at.Exoneration)
+  Age.at.Crime, Age.at.Conviction, Age.at.Release, Age.at.Exoneration,
+  Age.Group.at.Crime, Age.Group.at.Conviction, 
+  Age.Group.at.Release, Age.Group.at.Exoneration)
 
 
 ### ================ ###
 ### Saving to CSV
 ### ================ ###
 
+write.csv(crime_table, file = "data/exonerees-crime-table-july2024.csv", row.names = FALSE)
 # tags
 write.csv(tags_conviction, file = "data/exonerees-tags-conviction-may2024.csv", row.names = FALSE)
 write.csv(tags_exoneration, file = "data/exonerees-tags-exoneration-may2024.csv", row.names = FALSE)
@@ -188,3 +260,4 @@ write.csv(exonerees_conviction, file = "data/exonerees-conviction-may2024.csv", 
 write.csv(exonerees_exoneration, file = "data/exonerees-exoneration-may2024.csv", row.names = FALSE)
 # everything (selected above)
 write.csv(exonerees, file = "data/exonerees-may2024.csv", row.names = FALSE)
+write.csv(exonerees %>% filter(Year.of.Crime >= 1989), file = "data/exonerees1989-may2024.csv", row.names = FALSE)
